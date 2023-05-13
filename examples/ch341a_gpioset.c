a7079edce0375225d7421e41c5a66cc797680edc
Daniel Palmer <daniel@0x0f.com>
2023-05-13 19:50:57 +0900

    F

--- examples/ch341a_gpioset.c
+++ examples/ch341a_gpioset.c
@@ -5,6 +5,7 @@
 
 #include <argtable2.h>
 #include <stdio.h>
+#include <string.h>
 #include <libch341a.h>
 #include <spi_controller.h>
 #include <gpio_controller.h>
@@ -16,12 +17,14 @@ int main (int argc, char **argv)
 	int ret = 0;
 
 	struct arg_lit *help, *version;
+	struct arg_rex *lines;
 	struct arg_end *end;
 
 	void *argtable[] = {
 			/* help */
 			help = arg_lit0("h", "help", "Display this message and exit"),
 			version = arg_lit0("v", "version", "Display the version and exit"),
+			lines = arg_rexn(NULL, NULL, "[0-9]*=[0-1]", "<offsetN>=<valueN>", 0, 64, 0, "Line number/value pairs"),
 			end = arg_end(1),
 	};
 
@@ -35,6 +38,32 @@ int main (int argc, char **argv)
 		arg_print_syntax(stdout, argtable, "\n");
 		arg_print_glossary(stdout, argtable, "  %-30s %s\n");
 		return 0;
+	}
+
+	struct line_value_pair {
+		int line;
+		int value;
+	};
+	struct line_value_pair *line_value_pairs;
+	if (lines->count == 0) {
+		printf("need some line/value pairs\n");
+		return 1;
+	}
+	else {
+		line_value_pairs = malloc(sizeof(*line_value_pairs) * lines->count);
+		if (!line_value_pairs)
+			return 1;
+
+		for (int i = 0; i < lines->count; i++) {
+			char *l = lines->sval[i];
+			char *v = strchr(l, '=');
+
+			*v = '\0';
+			v++;
+
+			line_value_pairs[i].line = (int) strtol(l, NULL, 0);
+			line_value_pairs[i].value = (int) strtol(v, NULL, 0);
+		}
 	}
 
 	void *ch341a_mfd_priv;
